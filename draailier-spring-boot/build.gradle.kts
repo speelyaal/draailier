2fe54d46f8ce7e6f6c7e4721d8f3c0aa3cbe3761
Ashok <ashok@speelyaal.com>
2020-08-21 11:31:08 +0200

    fix version

--- draailier-spring-boot/build.gradle.kts
+++ draailier-spring-boot/build.gradle.kts
@@ -1,7 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-group = "com.speelyaal"
 version = "1.0.0"
+
+
+group = "com.speelyaal"
+
 plugins {
 	id("org.springframework.boot") version "2.3.0.M2"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
